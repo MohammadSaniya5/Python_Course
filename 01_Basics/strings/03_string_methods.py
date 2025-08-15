e12e18b930bd74878bdee10dc8006d73f4903881
Mohammad Saniya <saniyamohammad0567@gmail.com>
2025-08-15 20:43:25 +0530

    Added Lesson 3: Common String Methods in Python

---
+++ 01_Basics/strings/03_string_methods.py
@@ -0,0 +1,10 @@
+# String Methods
+text = "   Python Programming   "
+
+print(text.strip())       # Remove spaces
+print(text.upper())       # Uppercase
+print(text.lower())       # Lowercase
+print(text.count("o"))    # Count 'o'
+print(text.replace("Python", "Java")) # Replace words
+print(text.title())       # Capitalize first letter of each word
+print(text.find("Prog"))  # Find substring
