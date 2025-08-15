154ed877e9c2ae7b390aefe3e8ad5e47286326ef
Mohammad Saniya <saniyamohammad0567@gmail.com>
2025-08-15 20:24:48 +0530

    Added Lesson 4 - Data Types in Python

---
+++ 01_Basics/data_types.py
@@ -0,0 +1,15 @@
+# Integer
+age = 21
+print("Age:", age, "| Type:", type(age))
+
+# Float
+pi = 3.14
+print("Pi:", pi, "| Type:", type(pi))
+
+# String
+name = "Mohammad Saniya"
+print("Name:", name, "| Type:", type(name))
+
+# Boolean
+is_student = True
+print("Is student?", is_student, "| Type:", type(is_student))
