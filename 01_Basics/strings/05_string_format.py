897e8961c9986ff042738e3234fc90ddd091bfde
Mohammad Saniya <saniyamohammad0567@gmail.com>
2025-08-15 20:45:18 +0530

    Added Lesson 5: Using format() method for String Formatting

---
+++ 01_Basics/strings/05_string_format.py
@@ -0,0 +1,6 @@
+# Using format() method
+name = "Saniya"
+age = 21
+print("My name is {} and I am {} years old.".format(name, age))
+print("My name is {0} and I am {1} years old.".format(name, age))
+print("My name is {n} and I am {a} years old.".format(n=name, a=age))
