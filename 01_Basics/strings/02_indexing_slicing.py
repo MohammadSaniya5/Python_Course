eb51126e8ad29ee9d85e94f3ae6674206ccb8a41
Mohammad Saniya <saniyamohammad0567@gmail.com>
2025-08-15 20:42:05 +0530

    Added Lesson 2: Indexing and Slicing in Python Strings

---
+++ 01_Basics/strings/02_indexing_slicing.py
@@ -0,0 +1,7 @@
+# Indexing and Slicing in Strings
+name = "Saniya"
+
+print(name[0])   # First character
+print(name[-1])  # Last character
+print(name[1:4]) # 'ani'
+print(name[::-1]) # Reverse string
