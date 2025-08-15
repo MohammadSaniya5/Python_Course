369c663cd6c2033ec0d1abe760ae4015b6e486fc
Mohammad Saniya <saniyamohammad0567@gmail.com>
2025-08-15 20:21:33 +0530

    Added Lesson 2 - Printing and Comments

---
+++ 01_Basics/printing_and_comments.py
@@ -0,0 +1,16 @@
+# Single-line comment example
+# Anything after # will be ignored by Python
+
+"""
+Multi-line comment example
+Can be used for explanations or documentation
+"""
+
+# Printing text
+print("Hello, Python!")
+
+# Printing numbers and results
+print(5 + 3)  
+
+# Printing multiple values
+print("My age is", 21)
