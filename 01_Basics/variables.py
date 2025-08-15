dbf103c187dfe107ed91a593a6c65aa67f6962e1
Mohammad Saniya <saniyamohammad0567@gmail.com>
2025-08-15 20:22:59 +0530

    Added Lesson 3 - Variables in Python

---
+++ 01_Basics/variables.py
@@ -0,0 +1,15 @@
+# Variables store data in memory
+
+# Storing a number
+age = 21
+
+# Storing text (string)
+name = "Mohammad Saniya"
+
+# Storing a decimal number (float)
+height = 5.9
+
+# Printing variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
