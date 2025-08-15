6de3549ad5c9e42f53c61a1ba2a489a1220ebca8
Mohammad Saniya <saniyamohammad0567@gmail.com>
2025-08-15 20:26:32 +0530

    Added Lesson 5 - Type Casting in Python

---
+++ 01_Basics/type_casting.py
@@ -0,0 +1,17 @@
+# Converting int to float
+num = 10
+num_float = float(num)
+print("Float:", num_float)
+
+# Converting float to int
+pi = 3.99
+pi_int = int(pi)
+print("Integer:", pi_int)
+
+# Converting number to string
+age = 21
+age_str = str(age)
+print("String:", age_str)
+
+# Combining text and numbers (need to cast to str)
+print("I am " + str(age) + " years old.")
