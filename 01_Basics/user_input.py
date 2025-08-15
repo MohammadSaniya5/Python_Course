a21741c4f00752b38a2773b7fe7e8c8edb23a4a3
Mohammad Saniya <saniyamohammad0567@gmail.com>
2025-08-15 20:27:17 +0530

    Added Lesson 6 - Taking User Input in Python

---
+++ 01_Basics/user_input.py
@@ -0,0 +1,11 @@
+# Taking input as string
+name = input("Enter your name: ")
+print("Hello,", name)
+
+# Taking integer input
+age = int(input("Enter your age: "))
+print("You are", age, "years old.")
+
+# Taking float input
+height = float(input("Enter your height in meters: "))
+print("Your height is", height, "m")
