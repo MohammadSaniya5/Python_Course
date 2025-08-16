c6c0ba1ed0dd207314dedf58a633655a215dc74a
Mohammad Saniya <saniyamohammad0567@gmail.com>
2025-08-16 11:50:35 +0530

    Added Lesson 4 :Using f-Strings in Python

--- 01_Basics/strings/04_f_strings.py
+++ 01_Basics/strings/04_f_strings.py
@@ -1,5 +1,9 @@
-# f-Strings Example
+# f-Strings Examples
 name = "Saniya"
-age = 21
+age = 20
+score = 95.6789
+
 print(f"My name is {name} and I am {age} years old.")
-print(f"Next year, I will be {age + 1} years old.")
+print(f"Next year I will be {age + 1}.")
+print(f"My score is {score:.2f}")  # Format to 2 decimal places
+print(f"Binary of 10 is {10:b}")   # Binary format
