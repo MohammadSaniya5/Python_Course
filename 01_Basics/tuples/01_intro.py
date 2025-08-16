5a0158105aae590551cde02c5b8f3d9482747a83
Mohammad Saniya <saniyamohammad0567@gmail.com>
2025-08-16 12:13:07 +0530

    Added Lesson 1: Introduction to Tuples in Python

---
+++ 01_Basics/tuples/01_intro.py
@@ -0,0 +1,17 @@
+# Introduction to Tuples in Python
+
+# Creating tuples
+fruits = ("apple", "banana", "cherry")
+numbers = (1, 2, 3, 4, 5)
+mixed = ("hello", 10, 3.5, True)
+
+print("Fruits:", fruits)
+print("Numbers:", numbers)
+print("Mixed:", mixed)
+
+# Accessing elements
+print("First fruit:", fruits[0])
+print("Last number:", numbers[-1])
+
+# Tuples are immutable (cannot be changed)
+# fruits[0] = "grape"  # This will give an error
