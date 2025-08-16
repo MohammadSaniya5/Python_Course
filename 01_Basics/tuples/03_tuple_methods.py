f1bfd01cb7cb1bdf484a2de916d0e965ae04d3bf
Mohammad Saniya <saniyamohammad0567@gmail.com>
2025-08-16 12:24:43 +0530

    Added Lesson 3: Tuple Methods (count, index)

---
+++ 01_Basics/tuples/03_tuple_methods.py
@@ -0,0 +1,6 @@
+# Tuple Methods
+
+numbers = (10, 20, 30, 20, 40, 20)
+
+print("Count of 20:", numbers.count(20))  # How many times 20 appears
+print("Index of 30:", numbers.index(30))  # First occurrence of 30
