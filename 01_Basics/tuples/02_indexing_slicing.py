927412725c0f3e38020ac6df76c56475c0d74c0f
Mohammad Saniya <saniyamohammad0567@gmail.com>
2025-08-16 12:16:58 +0530

    Added Lesson 2: Indexing and Slicing in Tuples

---
+++ 01_Basics/tuples/02_indexing_slicing.py
@@ -0,0 +1,8 @@
+# Indexing and Slicing Tuples
+
+colors = ("red", "green", "blue", "yellow", "black")
+
+print("First color:", colors[0])
+print("Last color:", colors[-1])
+print("Slice (1:4):", colors[1:4])
+print("Reversed:", colors[::-1])
